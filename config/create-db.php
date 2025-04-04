8dfde209bb37b4f6a2feffda119e197870f0a049
VincentTan8 <vincent.tan@wetalk.com>
2025-04-04 10:54:04 +0800

    feat: updated create-db with insert statements

--- config/create-db.php
+++ config/create-db.php
@@ -101,7 +101,7 @@ if ($conn->query($query) === TRUE) {
 $tablename = 'teacher';
 $query = "CREATE TABLE `$tablename` (
       	`id` INT AUTO_INCREMENT PRIMARY KEY,
-		`ref_num` VARCHAR(100) NOT NULL,
+		`ref_num` VARCHAR(100) NOT NULL UNIQUE,
 		`username` VARCHAR(100) UNIQUE,
       	`fname` VARCHAR(100) NOT NULL,
 		`mname` VARCHAR(100),
@@ -120,11 +120,15 @@ $query = "CREATE TABLE `$tablename` (
 ;
 if ($conn->query($query) === TRUE) {
 	echo "Table $tablename created successfully.<br>";
-	$query = "INSERT INTO `$tablename` 
-			(`fname`, `lname`, `username`, `email`, `city`, `phone`, `birthday`, `gender`, `language_id`, `password`) 
-			VALUES
-			('Kat', 'Gecolea', 'katgecolea', 'kat@wetalk.com', 'Cavite', '+639876543210', '1990-05-01', 'Female', 2, 'test')
-		;"
+	$query = 'INSERT INTO `teacher` (`id`, `ref_num`, `fname`, `mname`, `lname`, `email`, `city`, `phone`, `birthday`, `gender`, `language_id`, `password`) VALUES
+			(1, "WT-0004", "Kat", NULL, "Gecolea", "kat.gecolea@wetalk.com", "Taguig", "+639876543210", "1900-03-06", "Female", 2, "kat12@#"),
+			(2, "WT-0062", "Jolife", NULL, "Lauro", "jolife.lauro@wetalk.com", "Taguig", "+639959400919", "1996-04-02", "Female", 2, "jolife@@123"),
+			(3, "WT-0064", "Krizia", NULL, "Decena", "krizia.decena@wetalk.com", "Taguig", "", "1999-06-10", "Female", 2, "kriz12$e"),
+			(4, "WT-0009", "Rachelle", NULL, "Bodino", "rachelle.bodino@wetalk.com", "Pasig", "", "1988-08-27", "Female", 2, "raechelle12$%"),
+			(5, "WT-0021", "Jaser", NULL, "Placeros", "jaser.placeros@wetalk.com", "Taguig", "", "1997-10-15", "Female", 2, "jaser()90"),
+			(6, "WT-0067", "Mia", NULL, "Cordez", "mia.cordez@wetalk.com", "Taguig", "", "2001-12-11", "Female", 2, "mia*&^567"),
+			(7, "WT-0066", "Leeanne", NULL, "Sebastian", "leeanne.sebastian@wetalk.com", "Taguig", "", "2002-04-10", "Female", 2, "leeanne765?>("),
+			(8, "WT-0088", "Vincent", NULL, "Tan", "vincent@oseamatrix.com", "Taguig", "+639326286802", "1998-01-08", "Male", 1, "vince");'
 	;
 	$conn->query($query);
 } else {
@@ -135,7 +139,7 @@ if ($conn->query($query) === TRUE) {
 $tablename = 'parent';
 $query = "CREATE TABLE `$tablename` (
       	`id` INT AUTO_INCREMENT PRIMARY KEY,
-		`ref_num` VARCHAR(100) NOT NULL,
+		`ref_num` VARCHAR(100) NOT NULL UNIQUE,
 		`username` VARCHAR(100) UNIQUE,
       	`fname` VARCHAR(100) NOT NULL,
 		`mname` VARCHAR(100),
@@ -159,7 +163,7 @@ if ($conn->query($query) === TRUE) {
 $tablename = 'student';
 $query = "CREATE TABLE `$tablename` (
       	`id` INT AUTO_INCREMENT PRIMARY KEY,
-		`ref_num` VARCHAR(100) NOT NULL,
+		`ref_num` VARCHAR(100) NOT NULL UNIQUE,
 		`username` VARCHAR(100) UNIQUE,
       	`fname` VARCHAR(100) NOT NULL,
 		`mname` VARCHAR(100),
@@ -180,6 +184,11 @@ $query = "CREATE TABLE `$tablename` (
 ;
 if ($conn->query($query) === TRUE) {
 	echo "Table $tablename created successfully.<br>";
+	$query = "INSERT INTO $tablename (`id`, `ref_num`, `fname`, `mname`, `lname`, `email`, `city`, `phone`, `birthday`, `gender`, `password`) VALUES
+			(1, 'FT-0001', 'Jeff', NULL, 'Uy', 'jeff.uy@oseamatrix.com', 'Manila', '1', '2025-06-01', 'Male', 'jeff'),
+			(2, 'FT-0002', 'Vincent', NULL, 'Tan', 'vincentdytan8@gmail.com', '', '0932', NULL, '', 'good');"
+	;
+	$conn->query($query);
 } else {
 	echo "Error creating table `$tablename`: $conn->error <br>";
 }
@@ -190,7 +199,7 @@ $query = "CREATE TABLE `$tablename` (
 		`student_ref_num` VARCHAR(100) NOT NULL,
 		`teacher_ref_num` VARCHAR(100) NOT NULL,
 		PRIMARY KEY (student_ref_num, teacher_ref_num),
-		FOREIGN KEY (`student_ref_num`) REFERENCES `student`(`ref_num`) ON DELETE CASCADE
+		FOREIGN KEY (`student_ref_num`) REFERENCES `student`(`ref_num`) ON DELETE CASCADE,
 		FOREIGN KEY (`teacher_ref_num`) REFERENCES `teacher`(`ref_num`) ON DELETE CASCADE
 	);"
 ;
@@ -205,7 +214,7 @@ if ($conn->query($query) === TRUE) {
 $tablename = 'schedule';
 $query = "CREATE TABLE `$tablename` (
       	`id` INT AUTO_INCREMENT PRIMARY KEY,
-		`ref_num` VARCHAR(100) NOT NULL,
+		`ref_num` VARCHAR(100) NOT NULL UNIQUE,
 		`scheddate` DATE,
 		`schedstarttime` TIME,
 		`schedendtime` TIME,
@@ -227,12 +236,12 @@ if ($conn->query($query) === TRUE) {
 $tablename = 'booking';
 $query = "CREATE TABLE `$tablename` (
       	`id` INT AUTO_INCREMENT PRIMARY KEY,
-		`ref_num` VARCHAR(100) NOT NULL,
+		`ref_num` VARCHAR(100) NOT NULL UNIQUE,
 		`schedule_ref_num` VARCHAR(100) NOT NULL,
 		`student_ref_num` VARCHAR(100) NOT NULL,
 		`encoded_by` VARCHAR(100) NOT NULL,
-		FOREIGN KEY (`schedule_ref_num`) REFERENCES `schedule`(`ref_num`) ON DELETE SET NULL,
-		FOREIGN KEY (`student_ref_num`) REFERENCES `student`(`ref_num`) ON DELETE SET NULL
+		FOREIGN KEY (`schedule_ref_num`) REFERENCES `schedule`(`ref_num`) ON DELETE CASCADE,
+		FOREIGN KEY (`student_ref_num`) REFERENCES `student`(`ref_num`) ON DELETE CASCADE
 	);"
 ;
 //add foreign key fpr schedule
@@ -264,7 +273,7 @@ if ($conn->query($query) === TRUE) {
 $tablename = 'cs';
 $query = "CREATE TABLE `$tablename` (
       	`id` INT AUTO_INCREMENT PRIMARY KEY,
-		`ref_num` VARCHAR(100) NOT NULL,
+		`ref_num` VARCHAR(100) NOT NULL UNIQUE,
 		`username` VARCHAR(100) UNIQUE,
       	`fname` VARCHAR(100) NOT NULL,
 		`mname` VARCHAR(100),
@@ -280,11 +289,12 @@ $query = "CREATE TABLE `$tablename` (
 ;
 if ($conn->query($query) === TRUE) {
 	echo "Table $tablename created successfully.<br>";
-	$query = "INSERT INTO `$tablename` 
-			(`fname`, `lname`, `email`, `city`, `phone`, `birthday`, `gender`, `password`) 
-			VALUES
-			('Judy', 'Aplacador', 'judy@wetalk.com', 'Taguig', '+639182736450', '1997-04-30', 'Female', 'test')
-		;"
+	$query = "INSERT INTO $tablename (`id`, `ref_num`, `fname`, `mname`, `lname`, `email`, `city`, `phone`, `birthday`, `gender`, `password`) VALUES
+			(1, 'WT-0061', 'Judy', NULL, 'Aplacador', 'judyann.aplacador@wetalk.com', 'Taguig', '+639673882051', '1997-11-11', 'Female', 'judy_@123!'),
+			(2, 'WT-0056', 'Ma Glaidylle', NULL, 'Montenegro', 'ma.glaidylle.montenegro@wetalk.com', 'Paranaque', '+639177065864', '1999-02-05', 'Female', 'glai887%@'),
+			(3, 'WT-0057', 'Jenny', NULL, 'Ralla', 'jenny.ralla@wetalk.com', 'Taguig', '+639171681680', '1998-04-16', 'Female', 'jen)(#234'),
+			(4, 'WT-0065', 'Roni', NULL, 'Cruz', 'roni.cruz@wetalk.com', 'Taguig', '+639198646687', '1997-12-28', 'Female', 'roni23@#!'),
+			(5, 'WT-0099', 'Harry', NULL, 'Macaraig', 'harry.macaraig@wetalk.com', 'Cavite', '+639327654345', '2020-03-20', 'Male', 'hars');"
 	;
 	$conn->query($query);
 } else {
