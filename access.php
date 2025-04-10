dd2b21e3fcb93bd9ce9bd95d97c0e421f37431bd
VincentTan8 <vincent.tan@wetalk.com>
2025-04-10 16:21:08 +0800

    chore: auto format from save

--- access.php
+++ access.php
@@ -1,8 +1,9 @@
-<?php 
-function check_access($allowed_user_type) {
+<?php
+function check_access($allowed_user_type)
+{
   if (!isset($_SESSION['access']) || $_SESSION['access'] !== $allowed_user_type) {
-      echo "<script>alert('Access Denied, Redirecting...'); window.location.href = '../login.php';</script>";
-      exit();
+    echo "<script>alert('Access Denied, Redirecting...'); window.location.href = '../login.php';</script>";
+    exit();
   }
 }
 ?>
