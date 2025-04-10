84ce8c1c8e1647e1a35cc6cbe0e4687b2a86b30f
VincentTan8 <vincent.tan@wetalk.com>
2025-04-10 17:54:12 +0800

    feat: updated delete-booking to be variable based for table name

--- wteach/delete-booking.php
+++ wteach/delete-booking.php
@@ -1,11 +1,11 @@
 <?php
 include "../config/conf.php";
-include "t-conf.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["booking_ref_num"])) {
     $booking_ref_num = $_POST["booking_ref_num"];
 
-    $sql = "DELETE FROM `booking` WHERE `ref_num` = ?";
+    $bookingtable = $prefix . "_resources.`booking`";
+    $sql = "DELETE FROM $bookingtable WHERE `ref_num` = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $booking_ref_num);
 
