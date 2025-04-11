6a27d615235cfdbbca7668226f833eafc6e0da15
Akatsv <jeff.uy@oseamatrix.com>
2025-04-11 17:49:56 +0800

    feat: updated update profile of student with new fields

--- wstudent/updateProfile.php
+++ wstudent/updateProfile.php
@@ -1,32 +1,48 @@
 <?php
 include "../config/conf.php";
-include "s-conf.php";
+$ref_num = $_SESSION['ref_num'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the updated data from the form submission
-    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
-    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
-    $city = mysqli_real_escape_string($conn, $_POST['city']);
-    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
-    $birthday = mysqli_real_escape_string($conn, $_POST['birthday']);
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $nickname = $_POST['nickname'];
+    $phone = $_POST['phone'];
+    $city = $_POST['city'];
+    $gender = $_POST['gender'];
+    $birthday = $_POST['birthday'];
+    $age = $_POST['age'];
+    $nationality = $_POST['nationality'];
+    $bio = $_POST['bio'];
 
     $tablename = $prefix . "_resources.`student`";
-    $qb = mysqli_query($conn, "UPDATE $tablename SET fname='$fname', lname='$lname', phone='$phone', city='$city', gender='$gender', birthday='$birthday' WHERE email='$email'");
+    $sql = "UPDATE $tablename 
+            SET `fname` = ?, `lname` = ?, `email` = ?, `username` = ?, `nickname` = ?, `phone` = ?, `city` = ?, `gender` = ?, `birthday` = ?, `age` = ?, `nationality` = ?, `bio` = ? 
+            WHERE `ref_num` = ?";
 
-    if ($qb) {
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssssssssss", $fname, $lname, $email, $username, $nickname, $phone, $city, $gender, $birthday, $age, $nationality, $bio, $ref_num);
+
+    if ($stmt->execute()) {
         // Send back the updated data as a JSON response
         echo json_encode([
             'success' => true,
             'fname' => $fname,
             'lname' => $lname,
             'email' => $email,
+            'username' => $username,
+            'nickname' => $nickname,
             'phone' => $phone,
             'gender' => $gender,
             'city' => $city,
-            'birthday' => $birthday
+            'birthday' => $birthday,
+            'age' => $age,
+            'nationality' => $nationality,
+            'bio' => $bio
         ]);
+        $stmt->close();
     } else {
         echo json_encode(['success' => false]);
     }
