804e08d3ee863379ff4e84fc61528f559eb54eb1
Akatsv <jeff.uy@oseamatrix.com>
2025-04-15 08:51:16 +0800

    feat: updated s-conf to use session variables

--- wstudent/s-conf.php
+++ wstudent/s-conf.php
@@ -12,18 +12,26 @@ if (isset($_SESSION['email']) && !empty($_SESSION['email'])) {
 	$tablename = $prefix . "_resources.`student`";
 	$student = $conn->query("SELECT * FROM $tablename WHERE `email` = '$email' LIMIT 1;");
 
-	for ($i = 0; $i < $student->num_rows; $i++) {
-		$row = $student->fetch_assoc();
-		$fname = $row["fname"];
-		$lname = $row["lname"];
-		$email = $row["email"];
-		$city = $row["city"];
-		$phone = $row["phone"];
-		$gender = $row["gender"];
-		$birthday = $row["birthday"];
-		$id = $row["id"];
-		$ref_num = $row["ref_num"];
-		$_SESSION['user_email'] = $row['email'];  // You can store email or other details as needed
+	if ($student) {
+		for ($i = 0; $i < $student->num_rows; $i++) {
+			$row = $student->fetch_assoc();
+			$_SESSION['username'] = $row['username'];
+			$_SESSION['fname'] = $row['fname'];
+			$_SESSION['lname'] = $row['lname'];
+			$_SESSION['nickname'] = $row['nickname'];
+			$_SESSION['bio'] = $row['bio'];
+			$_SESSION['city'] = $row['city'];
+			$_SESSION['phone'] = $row['phone'];
+			$_SESSION['gender'] = $row['gender'];
+			$_SESSION['birthday'] = $row['birthday'];
+			$_SESSION['age'] = $row['age'];
+			$_SESSION['nationality'] = $row['nationality'];
+			$_SESSION['parent_ref_num'] = $row['parent_ref_num'];
+			$_SESSION['id'] = $row['id'];
+			$_SESSION['ref_num'] = $row['ref_num'];
+		}
+	} else {
+
 	}
 }
 ?>
