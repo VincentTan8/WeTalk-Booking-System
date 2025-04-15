91317a2274729b2371ee3920196d523c1aaf38e5
Akatsv <jeff.uy@oseamatrix.com>
2025-04-15 10:08:30 +0800

    feat: design edits for sched trial button container and reference updates

--- wstudent/class.php
+++ wstudent/class.php
@@ -1,4 +1,13 @@
 <?php
+if (!isset($_SESSION)) {
+    session_start();
+    ob_start();
+}
+//todo remove conf and other fetch statements here. Move them into their own fetch files
+include "../config/conf.php";
+?>
+
+<?php
 $current = 'class';
 ?>
 <div class="container">
@@ -9,7 +18,7 @@ $current = 'class';
     $studenttable = $prefix . "_resources.`student`";
     $scheduletable = $prefix . "_resources.`schedule`";
     $teachertable = $prefix . "_resources.`teacher`";
-
+    $ref_num = $_SESSION['ref_num'];
     // Booking check
     $booking_check_sql = "SELECT COUNT(*) as booking_count FROM $bookingtable WHERE student_ref_num = '$ref_num'";
     $booking_check_result = $conn->query($booking_check_sql);
@@ -20,13 +29,11 @@ $current = 'class';
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" />
     <link rel="stylesheet" type="text/css"
         href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css" />
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
 
     <div class="row mt-4">
-        <div class="col-9" style="margin-bottom:20px;">
+        <div class="col-12 col-md-12 col-lg-9" style="margin-bottom:20px;">
             <?php if ($booking_count > 0): ?>
                 <div class="p-3 bg-white rounded">
                     <?php
@@ -56,7 +63,7 @@ $current = 'class';
 
                     $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
-                        echo "<table id='bookingTable' class='display' style='width:100%;'>";
+                        echo "<table id='bookingTable' class='display' style='width:100%; '>";
                         echo "<thead>
                         <tr>
                             <th style='background-color: #FFAC00; color: white;'>Teacher Name</th>
@@ -92,21 +99,17 @@ $current = 'class';
                     }
                     ?>
                 </div>
-
-                <!-- Trial scheduling shown only if booking exists -->
-                <div class="mt-3">
-                    <?php include "sched-trial.php"; ?>
-                </div>
-            <?php else: ?>
-                <!-- Show only sched-trial if there are no bookings -->
-                <div class="p-3 bg-white rounded">
-                    <?php include "sched-trial.php"; ?>
-                </div>
             <?php endif; ?>
+
+            <div class="mt-3">
+                <div class="trial-class-container  p-3 rounded">
+                    <?php include "sched-trial.php"; ?>
+                </div>
+            </div>
         </div>
 
         <div class="col-lg-3 col-md-12 minical-container">
-            <?php include "../utils/minical.php"; ?>
+            <?php include "../utils/sidebar.php"; ?>
         </div>
     </div>
 
