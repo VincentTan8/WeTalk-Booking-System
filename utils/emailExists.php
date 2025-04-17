67ba833cfe6ff4cb0240f14c25657ce2f23ca224
VincentTan8 <vincent.tan@wetalk.com>
2025-04-17 12:44:41 +0800

    feat: added a condition for empty string email input

--- utils/emailExists.php
+++ utils/emailExists.php
@@ -1,29 +1,30 @@
 <?php
 function emailExists($conn, $email, $userTables, $ref_num)
 {
-    $tables = $userTables;
+    //if empty string just ignore and return false
+    if (trim($email) !== '') {
+        $tables = $userTables;
 
-    foreach ($tables as $table) {
-        $query = "SELECT `ref_num` FROM `$table` WHERE `email` = ? LIMIT 1";
-        $stmt = $conn->prepare($query);
-        if (!$stmt)
-            continue;
+        foreach ($tables as $table) {
+            $query = "SELECT `ref_num` FROM `$table` WHERE `email` = ? LIMIT 1";
+            $stmt = $conn->prepare($query);
+            if (!$stmt)
+                continue;
 
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            if (!($ref_num === $row['ref_num'])) {
-                $stmt->close();
-                return true; // Found a match
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                if (!($ref_num === $row['ref_num'])) {
+                    $stmt->close();
+                    return true; // Found a match
+                }
             }
+            $stmt->close();
         }
-
-        $stmt->close();
     }
-
     return false; // No match found in any table
 }
 ?>
