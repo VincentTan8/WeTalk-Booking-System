08be83ced77343b6e1c60055b244ad3b621441b4
VincentTan8 <vincent.tan@wetalk.com>
2025-04-17 17:57:24 +0800

    refactor: moved fetch logic outside of class.php and temporarily removed booking count check, refactored most of the code to match how teacher table is generated using s-data.php file

--- wstudent/class.php
+++ wstudent/class.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION)) {
     session_start();
     ob_start();
 }
-//todo remove conf and other fetch statements here. Move them into their own fetch files
-include "../config/conf.php";
+//todo fix booking count
+// // Booking check
+// $booking_check_sql = "SELECT COUNT(*) as booking_count FROM $bookingtable WHERE student_ref_num = '$ref_num'";
+// $booking_check_result = $conn->query($booking_check_sql);
+// $booking_count = $booking_check_result->fetch_assoc()['booking_count'];
 ?>
 
 <?php
@@ -12,19 +15,7 @@ $current = 'class';
 ?>
 <div style="display: grid">
     <div class="col-9" style="justify-self: center;">
-        <?php include "header.php";
-
-        // Table names
-        $bookingtable = $prefix . "_resources.`booking`";
-        $studenttable = $prefix . "_resources.`student`";
-        $scheduletable = $prefix . "_resources.`schedule`";
-        $teachertable = $prefix . "_resources.`teacher`";
-        $ref_num = $_SESSION['ref_num'];
-        // Booking check
-        $booking_check_sql = "SELECT COUNT(*) as booking_count FROM $bookingtable WHERE student_ref_num = '$ref_num'";
-        $booking_check_result = $conn->query($booking_check_sql);
-        $booking_count = $booking_check_result->fetch_assoc()['booking_count'];
-        ?>
+        <?php include "header.php" ?>
 
         <!-- Include DataTables CSS & JS -->
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" />
@@ -34,74 +25,25 @@ $current = 'class';
         <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
 
         <div class="row mt-4">
+            <!-- todo compare with student.php in teacher for col-md-12 -->
             <div class="col-12 col-md-12 col-lg-9" style="margin-bottom:20px;">
-                <?php if ($booking_count > 0): ?>
-                    <div class="p-3 bg-white rounded">
-                        <?php
-                        $student_ref_num = $ref_num;
-                        $sql = "SELECT concat(student.fname, ' ', student.lname) AS student_name, 
-                           concat(teacher.fname, ' ', teacher.lname) AS teacher_name, 
-                           schedule.schedstarttime, 
-                           schedule.schedendtime,
-                           schedule.scheddate, 
-                           schedule.platform,
-                    CASE 
-                        WHEN NOW() BETWEEN 
-                            STR_TO_DATE(CONCAT(scheddate, ' ', schedstarttime), '%Y-%m-%d %H:%i:%s') 
-                            AND 
-                            STR_TO_DATE(CONCAT(scheddate, ' ', schedendtime), '%Y-%m-%d %H:%i:%s') 
-                        THEN 'In Progress'
-                        WHEN NOW() > 
-                            STR_TO_DATE(CONCAT(scheddate, ' ', schedendtime), '%Y-%m-%d %H:%i:%s') 
-                        THEN 'Finished'
-                        ELSE 'Upcoming'
-                    END AS status 
-                     FROM $bookingtable
-            JOIN $studenttable ON booking.student_ref_num = student.ref_num
-            JOIN $scheduletable ON booking.schedule_ref_num = schedule.ref_num
-            JOIN $teachertable ON schedule.teacher_ref_num = teacher.ref_num
-            WHERE student.ref_num = '$student_ref_num'";
+                <div class="p-3 bg-white rounded">
 
-                        $result = $conn->query($sql);
-                        if ($result->num_rows > 0) {
-                            echo "<table id='bookingTable' class='display' style='width:100%; '>";
-                            echo "<thead>
-                        <tr>
-                            <th style='background-color: #FFAC00; color: white;'>Teacher Name</th>
-                            <th style='background-color: #FFAC00; color: white;'>Schedule Date</th>
-                            <th style='background-color: #FFAC00; color: white;'>Start Time</th>
-                            <th style='background-color: #FFAC00; color: white;'>End Time</th>
-                            <th style='background-color: #FFAC00; color: white;'>Platform</th>
-                            <th style='background-color: #FFAC00; color: white;'>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>";
-
-                            while ($row = $result->fetch_assoc()) {
-                                $platformText = ($row['platform'] == 1) ? "Online" : "Offline";
-                                $statusColor = match ($row['status']) {
-                                    'Upcoming' => 'color: red;',
-                                    'In Progress' => 'color: orange;',
-                                    'Finished' => 'color: green;',
-                                    default => ''
-                                };
-                                echo "<tr>
-                            <td>{$row['teacher_name']}</td>
-                            <td>{$row['scheddate']}</td>
-                            <td>{$row['schedstarttime']}</td>
-                            <td>{$row['schedendtime']}</td>
-                            <td>$platformText</td>
-                            <td><span style='$statusColor'>{$row['status']}</span></td>
-                        </tr>";
-                            }
-                            echo "</tbody></table>";
-                        } else {
-                            echo "No bookings found.";
-                        }
-                        ?>
-                    </div>
-                <?php endif; ?>
-
+                    <table id='bookingTable' class='display' style='width:100%;'>
+                        <thead>
+                            <tr>
+                                <th style="background-color: white;"></th> <!-- Column for delete button -->
+                                <th style='background-color: #FFAC00; color: white;'>Teacher Name</th>
+                                <th style='background-color: #FFAC00; color: white;'>Schedule Date</th>
+                                <th style='background-color: #FFAC00; color: white;'>Start Time</th>
+                                <th style='background-color: #FFAC00; color: white;'>End Time</th>
+                                <th style='background-color: #FFAC00; color: white;'>Platform</th>
+                                <th style='background-color: #FFAC00; color: white;'>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
                 <div class="mt-3">
                     <div class="trial-class-container  p-3 rounded">
                         <?php include "sched-trial.php"; ?>
@@ -113,20 +55,64 @@ $current = 'class';
                 <?php include "../utils/sidebar.php"; ?>
             </div>
         </div>
-
-        <script>
-            $(document).ready(function () {
-                $('#bookingTable').DataTable({
-                    "paging": true,
-                    "searching": true,
-                    "ordering": true,
-                    "info": true,
-                    "scrollX": true,
-                    "responsive": true
-                });
-            });
-        </script>
-
-        <script src="minical.js"></script>
     </div>
 </div>
+
+<script>
+    $(document).ready(function () {
+        let table = $('#bookingTable').DataTable({
+            "paging": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "scrollX": true,
+            "responsive": true,
+            "ajax": {
+                "url": "s-data.php", // Fetch data dynamically
+                "dataSrc": ""
+            },
+            "columns": [
+                {
+                    "data": "booking_ref_num",
+                    "render": function (data, type, row) {
+                        return `<button class="delete-btn" data-refnum="${data}" style="background:none;border:none;color:red;cursor:pointer;">❌</button>`;
+                    },
+                    "orderable": false
+                },
+                { "data": "teacher_name" },
+                { "data": "scheddate" },
+                { "data": "schedstarttime" },
+                { "data": "schedendtime" },
+                { "data": "platform" },
+                {
+                    "data": "status",
+                    "render": function (data, type, row) {
+                        return `<span style="color: ${row.statusColor};">${data}</span>`;
+                    }
+                }
+            ]
+        });
+
+        // Delete button click event
+        $('#bookingTable tbody').on('click', '.delete-btn', function () {
+            let b_ref_num = $(this).data('refnum');
+
+            if (confirm("Are you sure you want to delete this booking?")) {
+                $.ajax({
+                    url: '../utils/delete-booking.php',
+                    type: 'POST',
+                    data: { booking_ref_num: b_ref_num },
+                    success: function (response) {
+                        alert(response);
+                        table.ajax.reload(null, false); // Refresh table
+                    },
+                    error: function () {
+                        alert("Error deleting booking.");
+                    }
+                });
+            }
+        });
+    });
+</script>
+
+<script src="minical.js"></script>
