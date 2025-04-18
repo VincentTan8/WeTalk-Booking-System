787c49f84637046b6622d4ac7dd160bb129ac854
VincentTan8 <vincent.tan@wetalk.com>
2025-04-18 11:28:48 +0800

    feat: added constant.js file for modules

---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+export const months = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+export function formatDate(date) {
+    // Get the individual components
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    // Construct the desired format
+    return `${year}-${month}-${day}`;
+}
