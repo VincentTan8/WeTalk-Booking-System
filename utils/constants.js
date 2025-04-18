17230a70e8e4a809cf5b17decaa0899358f0fa3f
VincentTan8 <vincent.tan@wetalk.com>
2025-04-18 16:58:37 +0800

    fix: modified formatDate function to create new Date object based on parameter to avoid having to accommodate every date format

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,9 @@
 export const months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 
-export function formatDate(date) {
+export function formatDate(givenDate) {
     // Get the individual components
+    const date = new Date(givenDate);
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
     const day = String(date.getDate()).padStart(2, '0');
