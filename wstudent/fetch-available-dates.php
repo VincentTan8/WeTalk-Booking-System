f2d566f531b077c009b684304015e3e054f4e33d
VincentTan8 <vincent.tan@wetalk.com>
2025-04-21 17:16:15 +0800

    feat: added platform and language id in querying available dates

--- wstudent/fetch-available-dates.php
+++ wstudent/fetch-available-dates.php
@@ -3,21 +3,27 @@
 //and are after 30 minutes of current time
 require '../config/conf.php';
 
-// if (!isset($_POST['platform']) || !isset($_POST['language_id'])) {
-//     echo json_encode(["error" => "Platform/Language not provided"]);
-//     exit;
-// }
+if (!isset($_POST['platform']) || !isset($_POST['language_id'])) {
+    echo json_encode(["error" => "Platform/Language not provided"]);
+    exit;
+}
 
-// $platform = $_POST['platform'];
-// $language_id = $_POST['language_id'];
+$platform = $_POST['platform'];
+$language_id = $_POST['language_id'];
 
 $scheduletable = $prefix . "_resources.`schedule`";
 $query = "SELECT DISTINCT `scheddate` 
             FROM $scheduletable 
             WHERE STR_TO_DATE(CONCAT(scheddate, ' ', schedstarttime), '%Y-%m-%d %H:%i:%s') >= DATE_ADD(NOW(), INTERVAL 30 MINUTE) 
+            AND (`platform` = ? OR `platform` = 2)
+            AND `language_id` = ?
             AND `booking_ref_num` IS NULL  
             ORDER BY `scheddate` ASC";
-$result = $conn->query($query);
+
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ii", $platform, $language_id);
+$stmt->execute();
+$result = $stmt->get_result();
 
 $dates = [];
 while ($row = $result->fetch_assoc()) {
