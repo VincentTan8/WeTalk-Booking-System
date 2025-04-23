c2c097623f4c3a2880f87ff02b5e1d1de519b693
VincentTan8 <vincent.tan@wetalk.com>
2025-04-23 17:39:25 +0800

    refactor: removed duplicate sections of calendar js and added new condition to highlight class

--- wcs/calendar.js
+++ wcs/calendar.js
@@ -1,3 +1,4 @@
+// Toggle button functionality
 const toggleButtons = document.querySelectorAll(".toggle-btn");
 let type = "online";
 
@@ -16,9 +17,9 @@ const selectButton = (selectedButton) => {
 };
 toggleButtons.forEach(button => {
     button.addEventListener("click", () => selectButton(button));
-
 });
 
+//Language toggle above calendar
 const calLanguageSelect = document.getElementById("calLanguageSelect");
 const fetchLanguages = async () => {
     try {
@@ -41,21 +42,6 @@ const fetchLanguages = async () => {
 };
 fetchLanguages();
 
-
-toggleOnline.addEventListener("change", function () {
-    if (this.checked) {
-        type = "online";
-        renderCalendar(type);
-    }
-});
-
-toggleOffline.addEventListener("change", function () {
-    if (this.checked) {
-        type = "offline";
-        renderCalendar(type);
-    }
-});
-
 const daysTag = document.querySelector(".days"),
     currentDateElement = document.querySelector(".current-date"),
     prevNextIcon = document.querySelectorAll(".icons span");
@@ -69,126 +55,60 @@ const months = ["January", "February", "March", "April", "May", "June", "July",
 
 let schedules = []; //initialize schedules
 
-const addLastDays = (lastDayOfMonth, liTag) => {
-    for (let i = lastDayOfMonth; i < 6; i++) {
-        liTag += `<li class="inactive" style="pointer-events: none; opacity: 0.5;">${i - lastDayOfMonth + 1}</li>`;
-    }
-    daysTag.innerHTML = liTag; //for the document query selector on li click events to work
-}
-
 const renderCalendar = async (type) => {
+    if (type === "online") {
+        //render calendar for showing online free schedules
+        await fetch('fetch-online-schedules.php')
+            .then(response => response.json())
+            .then(data => {
+                schedules = data;
+            })
+            .catch(error => console.error('Error fetching online schedules:', error));
+    } else if (type === "offline") {
+        //render calendar for showing offline free schedules
+        await fetch('fetch-offline-schedules.php')
+            .then(response => response.json())
+            .then(data => {
+                schedules = data;
+            })
+            .catch(error => console.error('Error fetching offline schedules:', error));
+    }
+
     let liTag = "";
     const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(),
         lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(),
         lastDateOfPrevMonth = new Date(currYear, currMonth, 0).getDate(),
         lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay();
 
+    //add days of prev month
     for (let i = firstDayOfMonth; i > 0; i--) {
         liTag += `<li class="inactive" style="pointer-events: none; opacity: 0.5;">${lastDateOfPrevMonth - i + 1}</li>`;
     }
 
-    if (type === "free") {
-        //gets free schedules, does not include booked schedules
-        await fetch('fetch-free-sched.php')
-            .then(response => response.json())
-            .then(data => {
-                schedules = data;
-            })
-            .catch(error => console.error('Error fetching schedules:', error));
+    //add days of current month
+    for (let i = 1; i <= lastDateOfMonth; i++) {
+        const fullDate = `${currYear}-${String(currMonth + 1).padStart(2, '0')}-${String(i).padStart(2, '0')}`;
+        const isScheduled = schedules.some(s => s.scheddate === fullDate);
+        const isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear();
 
-        for (let i = 1; i <= lastDateOfMonth; i++) {
-            const fullDate = `${currYear}-${String(currMonth + 1).padStart(2, '0')}-${String(i).padStart(2, '0')}`;
-            const isScheduled = schedules.some(s => s.scheddate === fullDate);
-            const isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear();
+        // Check if the date is before today
+        const currentDate = new Date();
+        const compareDate = new Date(currYear, currMonth, i);
+        const isPast = compareDate < currentDate.setHours(0, 0, 0, 0);
 
-            // Check if the date is before today
-            const currentDate = new Date();
-            const compareDate = new Date(currYear, currMonth, i);
-            const isPast = compareDate < currentDate.setHours(0, 0, 0, 0);
+        // TODO: consider changing which class should apply first since free sched today that are past the time does not display on the modal
+        const highlightClass = isScheduled ? "scheduled" : isPast ? "past" : "";
+        // const highlightClass = isScheduled ? "scheduled" : isToday ? "active" : "";
 
-            // TODO: consider changing which class should apply first since free sched today that are past the time does not display on the modal
-            const highlightClass = isScheduled ? "scheduled" : isPast ? "past" : "";
-            // const highlightClass = isScheduled ? "scheduled" : isToday ? "active" : "";
-
-            liTag += `<li class="${highlightClass}" data-date="${fullDate}" ${isPast ? 'style="pointer-events: none; opacity: 0.5;"' : ""}>${i}</li>`;
-            // liTag += `<li class="${highlightClass}" data-date="${fullDate}">${i}</li>`;
-        }
-
-        addLastDays(lastDayOfMonth, liTag);
-
-        document.querySelectorAll(".days li").forEach(day => {
-            if (!day.classList.contains("inactive") && day.classList.contains("scheduled")) {
-                day.addEventListener("click", () => {
-                    //reset offline/online selection
-                    document.querySelectorAll('input[name="platform"]').forEach(platform => {
-                        platform.checked = false;
-                    })
-
-                    const selectedDate = day.getAttribute("data-date");
-                    const modal = new bootstrap.Modal(document.getElementById('submissionModal'));
-                    modal.show();
-                });
-            }
-        });
-    } else if (type === "online") {
-        //render calendar for showing online booked schedules
-        //gets online bookings only
-        await fetch('fetch-online-booking.php')
-            .then(response => response.json())
-            .then(data => {
-                schedules = data;
-            })
-            .catch(error => console.error('Error fetching online bookings:', error));
-
-        for (let i = 1; i <= lastDateOfMonth; i++) {
-            const fullDate = `${currYear}-${String(currMonth + 1).padStart(2, '0')}-${String(i).padStart(2, '0')}`;
-            const isScheduled = schedules.some(s => s.scheddate === fullDate);
-            const isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear();
-
-            const highlightClass = isScheduled ? "scheduled" : "";
-
-            liTag += `<li class="${highlightClass}" data-date="${fullDate}">${i}</li>`;
-        }
-
-        addLastDays(lastDayOfMonth, liTag);
-
-        document.querySelectorAll(".days li").forEach(day => {
-            if (!day.classList.contains("inactive") && day.classList.contains("scheduled")) {
-                day.addEventListener("click", () => {
-                    //reset offline/online selection
-                    document.querySelectorAll('input[name="platform"]').forEach(platform => {
-                        platform.checked = false;
-                    })
-
-                    const selectedDate = day.getAttribute("data-date");
-                    const modal = new bootstrap.Modal(document.getElementById('submissionModal'));
-                    modal.show();
-                });
-            }
-        });
-    } else if (type === "offline") {
-        //render calendar for showing offline booked schedules
-        //gets offline bookings only
-        await fetch('fetch-offline-booking.php')
-            .then(response => response.json())
-            .then(data => {
-                schedules = data;
-            })
-            .catch(error => console.error('Error fetching offline bookings:', error));
-
-        for (let i = 1; i <= lastDateOfMonth; i++) {
-            const fullDate = `${currYear}-${String(currMonth + 1).padStart(2, '0')}-${String(i).padStart(2, '0')}`;
-            const isScheduled = schedules.some(s => s.scheddate === fullDate);
-            const isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear();
-
-            const highlightClass = isScheduled ? "scheduled" : "";
-
-            liTag += `<li class="${highlightClass}" data-date="${fullDate}">${i}</li>`;
-        }
-
-        addLastDays(lastDayOfMonth, liTag);
-
+        liTag += `<li class="${highlightClass}" data-date="${fullDate}" ${isPast ? 'style="pointer-events: none; opacity: 0.5;"' : ''} ${!isScheduled ? 'style="pointer-events: none;"' : ''}>${i}</li>`;
+        // liTag += `<li class="${highlightClass}" data-date="${fullDate}">${i}</li>`;
     }
+
+    //add days of next month
+    for (let i = lastDayOfMonth; i < 6; i++) {
+        liTag += `<li class="inactive" style="pointer-events: none; opacity: 0.5;">${i - lastDayOfMonth + 1}</li>`;
+    }
+    daysTag.innerHTML = liTag; //for the document query selector on li click events to work
 
     currentDateElement.innerText = `${months[currMonth]} ${currYear}`;
 };
