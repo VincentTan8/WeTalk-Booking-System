8c837a006d12595d5224db5265514c0cd4c2033f
VincentTan8 <vincent.tan@wetalk.com>
2025-04-23 19:30:22 +0800

    refactor: changed fetch online/offline schedules to be based on post vars such as platform and language id

--- wcs/fetch-schedules.php
+++ wcs/fetch-schedules.php
@@ -1,11 +1,21 @@
 <?php
-// <!-- Gets offline free schedules, not booked schedules -->
+// <!-- Gets free schedules based on platform and language -->
 include "../config/conf.php";
+
+$platform = $_POST['platform'];
+$language_id = $_POST['language_id'];
 
 $tablename = $prefix . "_resources.`schedule`";
 $sql = "SELECT `scheddate`, `schedstarttime`, `schedendtime`, `platform` 
-        FROM $tablename WHERE `booking_ref_num` IS NULL AND (`platform` = 0 OR `platform` = 2)";
-$result = $conn->query($sql);
+        FROM $tablename 
+        WHERE `booking_ref_num` IS NULL 
+        AND (`platform` = ? OR `platform` = 2)
+        AND `language_id` = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ii", $platform, $language_id);
+$stmt->execute();
+$result = $stmt->get_result();
 
 $schedules = [];
 if ($result->num_rows > 0) {
