40b72dc9e65978232cb9f7d6bbcd69d0c3e53fcb
VincentTan8 <vincent.tan@wetalk.com>
2025-07-25 09:09:51 +0800

    chore: temporary added delete booking capability for parent view

--- wparent/class.php
+++ wparent/class.php
@@ -10,12 +10,7 @@ $current = 'class';
 ?>
 <div style="display: grid">
     <div class="col-9" style="justify-self: center;">
-        <?php include "header.php";
-        // Booking check
-        // $booking_check_sql = "SELECT COUNT(*) as booking_count FROM $bookingtable b JOIN $studenttable s ON b.student_ref_num = s.ref_num WHERE s.parent_ref_num = '$parent_ref_num'";
-        // $booking_check_result = $conn->query($booking_check_sql);
-        // $booking_count = $booking_check_result->fetch_assoc()['booking_count'];
-        ?>
+        <?php include "header.php"; ?>
 
         <!-- Include DataTables CSS & JS -->
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" />
@@ -30,6 +25,7 @@ $current = 'class';
                     <table id='bookingTable' class='display' style='width:100%;'>
                         <thead>
                             <tr>
+                                <th class="highlight-parent"></th> <!-- Column for delete button -->
                                 <th class="highlight-parent" style="color: white;">Student Name</th>
                                 <th class="highlight-parent" style="color: white;">Teacher Name</th>
                                 <th class="highlight-parent" style="color: white;">Schedule Date</th>
@@ -70,7 +66,13 @@ $current = 'class';
                 "dataSrc": ""
             },
             "columns": [
-
+                {
+                    "data": "booking_ref_num",
+                    "render": function (data, type, row) {
+                        return `<button class="delete-btn" data-refnum="${data}" style="background:none;border:none;color:red;cursor:pointer;">❌</button>`;
+                    },
+                    "orderable": false
+                },
                 { "data": "student_name" },
                 { "data": "teacher_name" },
                 { "data": "scheddate" },
@@ -84,6 +86,26 @@ $current = 'class';
                     }
                 }
             ]
+        });
+
+        // Delete button click event
+        $('#bookingTable tbody').on('click', '.delete-btn', function () {
+            let b_ref_num = $(this).data('refnum');
+
+            if (confirm("Are you sure you want to delete this booking?")) {
+                $.ajax({
+                    url: '../utils/delete-booking.php',
+                    type: 'POST',
+                    data: { booking_ref_num: b_ref_num },
+                    success: function (response) {
+                        alert(response);
+                        table.ajax.reload(null, false); // Refresh table
+                    },
+                    error: function () {
+                        alert("Error deleting booking.");
+                    }
+                });
+            }
         });
     });
 </script>
