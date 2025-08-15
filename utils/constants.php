544afb342f6ad413dfb02dc842ca358c2a7d6068
VincentTan8 <vincent.tan@wetalk.com>
2025-08-15 15:25:43 +0800

    feat: added new usertable to constants php

--- utils/constants.php
+++ utils/constants.php
@@ -1,3 +1,4 @@
 <?php
-$userTables = ['student', 'teacher', 'cs', 'parent'];
+//Hierarchy matters here since if a teacher is given admin access, the admin table should be checked first so user type returned is one with higher permissions
+$userTables = ['admin', 'cs', 'teacher', 'parent', 'student'];
 ?>
