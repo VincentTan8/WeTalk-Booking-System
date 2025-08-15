0735043beb59faadb3fc3f7aab7617dff0d6117b
VincentTan8 <vincent.tan@wetalk.com>
2025-08-15 15:31:16 +0800

    feat: modified access php to view other pages as admin

--- access.php
+++ access.php
@@ -1,7 +1,13 @@
 <?php
-function check_access($allowed_user_type)
+function check_access($allowed_user_types)
 {
-  if (!isset($_SESSION['access']) || $_SESSION['access'] !== $allowed_user_type) {
+  // Convert single string to array for consistency
+  if (!is_array($allowed_user_types)) {
+    $allowed_user_types = [$allowed_user_types];
+  }
+
+  // Check if access is allowed
+  if (!isset($_SESSION['access']) || (!in_array($_SESSION['access'], $allowed_user_types) && $_SESSION['access'] !== 'admin')) {
     echo "<script>alert('Access Denied, Redirecting...'); window.location.href = '../login.php';</script>";
     exit();
   }
