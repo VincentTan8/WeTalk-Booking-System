a5d159fc2d0229583cb9a4e450b6806daaf825e6
VincentTan8 <vincent.tan@wetalk.com>
2025-11-14 09:33:48 +0800

    feat: added profile modal file

---
+++ utils/profile-modal.php
@@ -0,0 +1,14 @@
+<!-- Profile Modal -->
+<div id="profileModal" class="modal fade" tabindex="-1">
+    <div class="modal-dialog" style="max-width: 1442px">
+        <div class="modal-content p-4">
+            <div class="modal-header">
+                <h5 class="modal-title">Profile</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Insert content here -->
+            </div>
+        </div>
+    </div>
+</div>
