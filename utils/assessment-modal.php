8fde961f518c80b72637aae9b69e14b1eeec5338
VincentTan8 <vincent.tan@wetalk.com>
2026-03-06 11:20:09 +0800

    feat: assessment modal styling and labels added, hidden inputs for return URL and booking ref num added, included div area for listing existing files

--- utils/assessment-modal.php
+++ utils/assessment-modal.php
@@ -8,14 +8,27 @@
             </div>
             <div class="modal-body">
                 <!-- Insert other content here kung kailangan -->
-                <span>Student:</span> <span style="font-weight: 400" id="assess-modal-student-name"></span>
-                <br>
-                <span>Teacher:</span> <span style="font-weight: 400" id="assess-modal-teacher-name"></span>
+                <div style="margin-bottom: 10px;"><span id="assess-modal-student-label">Student:</span> <span
+                        style="font-weight: 400" id="assess-modal-student-name"></span></div>
 
-                <form action="" method="POST">
-                    <!-- To indicate where to go after adding schedule -->
-                    <input type="hidden" id="returnUrl" name="returnUrl" value="">
+                <div style="margin-bottom: 10px;"><span id="assess-modal-teacher-label">Teacher:</span> <span
+                        style="font-weight: 400" id="assess-modal-teacher-name"></span></div>
 
+                <form action="../utils/update-assessment.php" method="POST" enctype="multipart/form-data">
+                    <!-- Indicate where to go after adding assessment report -->
+                    <input type="hidden" id="assessmentReturnUrl" name="returnUrl" value="">
+                    <input type="hidden" id="bookingRefNum" name="bookingRefNum" value="">
+
+                    <div style="margin-bottom: 10px; display: flex; flex-direction: column; gap: 10px">
+                        <label for="assessmentReport">Assessment Report:</label>
+                        <textarea id="assessmentReport" name="assessmentReport"
+                            style="height: 500px; padding: 8px;"></textarea>
+
+                        <label id="assess-modal-file-label" for="assessmentFile">Upload File:</label>
+                        <input type="file" name="assessmentFile" id="assessmentFile">
+                        <div id="assessmentExistingFile"></div>
+
+                    </div>
                     <div class="text-center">
                         <input type="submit" value="Save" id="assessment-button"
                             style="border-radius: 10px; padding: 13px 54px; color:white; border:none;">
